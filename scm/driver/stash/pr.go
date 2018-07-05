80949364ae398763cfb4d8429e6e2a3be715ed71
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-07-04 23:17:12 -0700

    stash: implement get pr comment endpoint

--- scm/driver/stash/pr.go
+++ scm/driver/stash/pr.go
@@ -24,8 +24,12 @@ func (s *pullService) Find(ctx context.Context, repo string, number int) (*scm.P
 	return convertPullRequest(out), res, err
 }
 
-func (s *pullService) FindComment(context.Context, string, int, int) (*scm.Comment, *scm.Response, error) {
-	return nil, nil, scm.ErrNotSupported
+func (s *pullService) FindComment(ctx context.Context, repo string, number int, id int) (*scm.Comment, *scm.Response, error) {
+	namespace, name := scm.Split(repo)
+	path := fmt.Sprintf("rest/api/1.0/projects/%s/repos/%s/pull-requests/%d/comments/%d", namespace, name, number, id)
+	out := new(pullRequestComment)
+	res, err := s.client.do(ctx, "GET", path, nil, out)
+	return convertPullRequestComment(out), res, err
 }
 
 func (s *pullService) List(ctx context.Context, repo string, opts scm.PullRequestListOptions) ([]*scm.PullRequest, *scm.Response, error) {
@@ -47,6 +51,7 @@ func (s *pullService) ListChanges(ctx context.Context, repo string, number int, 
 }
 
 func (s *pullService) ListComments(context.Context, string, int, scm.ListOptions) ([]*scm.Comment, *scm.Response, error) {
+	// rest/api/1.0/projects/PRJ/repos/my-repo/pull-requests/1/activities
 	return nil, nil, scm.ErrNotSupported
 }
 
@@ -69,6 +74,8 @@ func (s *pullService) CreateComment(context.Context, string, int, *scm.CommentIn
 }
 
 func (s *pullService) DeleteComment(context.Context, string, int, int) (*scm.Response, error) {
+	// {"errors":[{"context":null,"message":"You are attempting to modify a comment based on out-of-date information.","exceptionName":"com.atlassian.bitbucket.comment.CommentOutOfDateException","currentVersion":1,"expectedVersion":0}]}
+	// rest/api/1.0/projects/PRJ/repos/my-repo/pull-requests/1/comments/3?version=0
 	return nil, scm.ErrNotSupported
 }
 
@@ -213,6 +220,52 @@ func convertPullRequest(from *pullRequest) *scm.PullRequest {
 			Name:   from.Author.User.DisplayName,
 			Email:  from.Author.User.EmailAddress,
 			Avatar: avatarLink(from.Author.User.EmailAddress),
+		},
+	}
+}
+
+type pullRequestComment struct {
+	Properties struct {
+		RepositoryID int `json:"repositoryId"`
+	} `json:"properties"`
+	ID      int    `json:"id"`
+	Version int    `json:"version"`
+	Text    string `json:"text"`
+	Author  struct {
+		Name         string `json:"name"`
+		EmailAddress string `json:"emailAddress"`
+		ID           int    `json:"id"`
+		DisplayName  string `json:"displayName"`
+		Active       bool   `json:"active"`
+		Slug         string `json:"slug"`
+		Type         string `json:"type"`
+		Links        struct {
+			Self []struct {
+				Href string `json:"href"`
+			} `json:"self"`
+		} `json:"links"`
+	} `json:"author"`
+	CreatedDate         int64         `json:"createdDate"`
+	UpdatedDate         int64         `json:"updatedDate"`
+	Comments            []interface{} `json:"comments"`
+	Tasks               []interface{} `json:"tasks"`
+	PermittedOperations struct {
+		Editable  bool `json:"editable"`
+		Deletable bool `json:"deletable"`
+	} `json:"permittedOperations"`
+}
+
+func convertPullRequestComment(from *pullRequestComment) *scm.Comment {
+	return &scm.Comment{
+		ID:      from.ID,
+		Body:    from.Text,
+		Created: time.Unix(from.CreatedDate/1000, 0),
+		Updated: time.Unix(from.UpdatedDate/1000, 0),
+		Author: scm.User{
+			Login:  from.Author.Slug,
+			Name:   from.Author.DisplayName,
+			Email:  from.Author.EmailAddress,
+			Avatar: avatarLink(from.Author.EmailAddress),
 		},
 	}
 }
