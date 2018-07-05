11c1cb97d8123d2a22fa90c571d4f852a863677f
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-07-05 16:57:43 -0700

    add gitea driver

---
+++ scm/driver/gitea/pr_test.go
@@ -0,0 +1,96 @@
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gitea
+
+import (
+	"context"
+	"testing"
+
+	"github.com/drone/go-scm/scm"
+)
+
+//
+// pull request sub-tests
+//
+
+func TestPullRequestFind(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, _, err := client.PullRequests.Find(context.Background(), "go-gitea/gitea", 1)
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+func TestPullRequestList(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, _, err := client.PullRequests.List(context.Background(), "go-gitea/gitea", scm.PullRequestListOptions{})
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+func TestPullRequestClose(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, err := client.PullRequests.Close(context.Background(), "go-gitea/gitea", 1)
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+func TestPullRequestMerge(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, err := client.PullRequests.Merge(context.Background(), "go-gitea/gitea", 1)
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+//
+// pull request change sub-tests
+//
+
+func TestPullRequestChanges(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, _, err := client.PullRequests.ListChanges(context.Background(), "go-gitea/gitea", 1, scm.ListOptions{})
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+//
+// pull request comment sub-tests
+//
+
+func TestPullRequestCommentFind(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, _, err := client.PullRequests.FindComment(context.Background(), "go-gitea/gitea", 1, 1)
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+func TestPullRequestCommentList(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, _, err := client.PullRequests.ListComments(context.Background(), "go-gitea/gitea", 1, scm.ListOptions{})
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+func TestPullRequestCommentCreate(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, _, err := client.PullRequests.CreateComment(context.Background(), "go-gitea/gitea", 1, &scm.CommentInput{})
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
+
+func TestPullRequestCommentDelete(t *testing.T) {
+	client, _ := New("https://try.gitea.io")
+	_, err := client.PullRequests.DeleteComment(context.Background(), "go-gitea/gitea", 1, 1)
+	if err != scm.ErrNotSupported {
+		t.Errorf("Expect Not Supported error")
+	}
+}
