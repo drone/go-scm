632a41db1842c6fa2222bf8091e54d81bbb4d39c
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-09-11 02:32:56 -0700

    enable gitlab webhooks

--- scm/driver/gitlab/gitlab.go
+++ scm/driver/gitlab/gitlab.go
@@ -36,6 +36,7 @@ func New(uri string) (*scm.Client, error) {
 	client.Repositories = &repositoryService{client}
 	client.Reviews = &reviewService{client}
 	client.Users = &userService{client}
+	client.Webhooks = &webhookService{client}
 	return client.Client, nil
 }
 
