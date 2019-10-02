446da4b4616882fe20d38fcc07ffcd960cabc3e4
James Strachan <james.strachan@gmail.com>
2019-10-02 14:27:49 +0100

    fix: better example for listing a PR or PRs

--- scm/factory/examples/pr/main.go
+++ scm/factory/examples/pr/main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/ghodss/yaml"
+	"github.com/jenkins-x/go-scm/scm"
 	"github.com/pkg/errors"
 
 	"github.com/jenkins-x/go-scm/scm/factory"
@@ -15,11 +16,37 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) < 3 {
+	if len(args) < 2 {
 		fmt.Printf("usage: org/repo prNumber")
 		return
 	}
 	repo := args[1]
+
+	client, err := factory.NewClientFromEnvironment()
+	if err != nil {
+		helpers.Fail(err)
+		return
+	}
+	ctx := context.Background()
+
+	if len(args) < 3 {
+		fmt.Printf("Getting PRs\n")
+
+		prs, _, err := client.PullRequests.List(ctx, repo, scm.PullRequestListOptions{})
+		if err != nil {
+			helpers.Fail(err)
+			return
+		}
+		for _, pr := range prs {
+			fmt.Printf("Found PullRequest:\n")
+			data, err := yaml.Marshal(pr)
+			if err != nil {
+				helpers.Fail(errors.Wrap(err, "failed to marshal PR as YAML"))
+				return
+			}
+			fmt.Printf("%s:\n", string(data))
+		}
+	}
 	prText := args[2]
 	number, err := strconv.Atoi(prText)
 	if err != nil {
@@ -27,15 +54,8 @@ func main() {
 		return
 	}
 
-	client, err := factory.NewClientFromEnvironment()
-	if err != nil {
-		helpers.Fail(err)
-		return
-	}
+	fmt.Printf("Getting PR\n")
 
-	fmt.Printf("Getting PRs\n")
-
-	ctx := context.Background()
 	pr, _, err := client.PullRequests.Find(ctx, repo, number)
 	if err != nil {
 		helpers.Fail(err)
