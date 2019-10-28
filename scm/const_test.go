drone/go-scm

commit fad5b44456ec8ba9c04161449d8ca37ab1a37ae9

Add JSON marshalling support for State codes.
This converts JSON serialization of statuses into their string
representation. e.g.

Old:
```
{
  "state": 3,
}
```

New:
```
{
  "state": "success",
}
```

scm/const.go
@@ -70,6 +70,15 @@ func ToState(s string) State {
 	}
 }
 
+func (s State) MarshalJSON() ([]byte, error) {
+	return []byte(s.String()), nil
+}
+
+func (s *State) UnmarshalJSON(b []byte) error {
+	*s = ToState(strings.Trim(string(b), `"`))
+	return nil
+}
+
 // Action identifies webhook actions.
 type Action int
 

scm/driver/bitbucket/testdata/status.json.golden
@@ -1,5 +1,5 @@
 {
-    "State": 3,
+    "State": "success",
     "Label": "drone",
     "Desc": "Build has completed successfully",
     "Target": "https://ci.example.com/1000/output"

scm/driver/bitbucket/testdata/statuses.json.golden
@@ -1,6 +1,6 @@
 [
     {
-        "State": 3,
+        "State": "success",
         "Label": "drone",
         "Desc": "Build has completed successfully",
         "Target": "https://ci.example.com/1000/output"

scm/driver/gitea/testdata/status.json.golden
@@ -1,5 +1,5 @@
 {
-    "State": 3,
+    "State": "success",
     "Label": "continuous-integration/drone",
     "Desc": "",
     "Target": "https://example.com"

scm/driver/gitea/testdata/statuses.json.golden
@@ -1,6 +1,6 @@
 [
     {
-        "State": 3,
+        "State": "success",
         "Label": "continuous-integration/drone",
         "Desc": "",
         "Target": "https://example.com"

scm/driver/github/testdata/combined_status.json.golden
@@ -1,15 +1,15 @@
 {
-    "State": 3,
+    "State": "success",
     "Sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
     "Statuses": [
         {
-            "State": 3,
+            "State": "success",
             "Label": "continuous-integration/jenkins",
             "Desc": "Build has completed successfully",
             "Target": "https://ci.example.com/1000/output"
         },
         {
-            "State": 3,
+            "State": "success",
             "Label": "security/brakeman",
             "Desc": "Testing has completed successfully",
             "Target": "https://ci.example.com/2000/output"

scm/driver/github/testdata/status.json.golden
@@ -1,5 +1,5 @@
 {
-    "State": 3,
+    "State": "success",
     "Label": "continuous-integration/drone",
     "Desc": "Build has completed successfully",
     "Target": "https://ci.example.com/1000/output"

scm/driver/github/testdata/statuses.json.golden
@@ -1,6 +1,6 @@
 [
     {
-        "State": 3,
+        "State": "success",
         "Label": "continuous-integration/drone",
         "Desc": "Build has completed successfully",
         "Target": "https://ci.example.com/1000/output"

commit 2dbc801930726e3ad02806fd954d889d57495031

Fix json.Marshal for State.
Fixes breakage when using json.Marshal introduced in
fad5b44456ec8ba9c04161449d8ca37ab1a37ae9.

Not entirely sure why the other tests in the previous commit succeeded, so also
makes sure this is tested explicitly.

scm/const.go
@@ -6,6 +6,7 @@ package scm
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 )
 
@@ -71,7 +72,7 @@ func ToState(s string) State {
 }
 
 func (s State) MarshalJSON() ([]byte, error) {
-	return []byte(s.String()), nil
+	return []byte(fmt.Sprintf(`"%s"`, s.String())), nil
 }
 
 func (s *State) UnmarshalJSON(b []byte) error {

scm/const_test.go
@@ -1,0 +1,27 @@
+package scm
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestStateJSON(t *testing.T) {
+	for i := StateUnknown; i < StateExpected; i++ {
+		in := State(i)
+		t.Run(in.String(), func(t *testing.T) {
+			b, err := json.Marshal(in)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			var out State
+			if err := json.Unmarshal(b, &out); err != nil {
+				t.Fatal(err)
+			}
+
+			if in != out {
+				t.Errorf("%s != %s", in, out)
+			}
+		})
+	}
+}