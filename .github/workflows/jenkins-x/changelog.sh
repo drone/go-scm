8eac2ee995d81652d84e757152fa5433b1a64064
James Strachan <james.strachan@gmail.com>
2021-03-10 11:17:23 +0000

    fix: add lighthouse release

--- .github/workflows/jenkins-x/changelog.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "REPO_NAME = $PULL_BASE_SHA"
-
-export PULL_BASE_SHA=$(git rev-parse HEAD)
-
-jx changelog create --verbose --version=$VERSION --rev=$PULL_BASE_SHA --output-markdown=changelog.md --update-release=false
-
