4619616f9fef7705ac977e8d079d1477346fd39a
Frazer Gibson <frazer.gibson@nttdata.com>
2021-03-25 14:35:06 +0000

    fixes

--- scm/driver/bitbucket/util.go
+++ scm/driver/bitbucket/util.go
@@ -86,7 +86,8 @@ func encodePullRequestListOptions(opts scm.PullRequestListOptions) string {
 		params.Set("page", strconv.Itoa(opts.Page))
 	}
 	if opts.Size != 0 {
-		params.Set("pagelen", strconv.Itoa(opts.Size))
+		// params.Set("pagelen", strconv.Itoa(opts.Size))
+		params.Set("pagelen", "50")
 	}
 	if opts.Open && opts.Closed {
 		params.Set("state", "all")
