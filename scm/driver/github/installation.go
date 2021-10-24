a3f33571ffd881138d489e37884530690f4829b0
Yshay Yaacobi <yshayy@gmail.com>
2021-10-24 13:02:42 +0000

    add support for getting app installations for authenticated user

--- scm/driver/github/installation.go
+++ scm/driver/github/installation.go
@@ -3,6 +3,7 @@ package github
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/livecycle/go-scm/scm"
 )
@@ -18,4 +19,41 @@ func (s *installationService) ListRepositories(ctx context.Context, opts scm.Lis
 	}
 	res, err := s.client.do(ctx, "GET", path, nil, &out)
 	return convertRepositoryList(out.Repositories), res, err
+}
+
+// Installation
+type installation struct {
+	Id      int           `json:"id"`
+	Account *organization `json:"account"`
+	AppId   int           `json:"app_id"`
+	AppSlug string        `json:"app_slug"`
+}
+
+func (s *installationService) ListInstallationsForAuthenticatedUser(ctx context.Context, opts scm.ListOptions) ([]*scm.Installation, *scm.Response, error) {
+	path := fmt.Sprintf("user/installations?%s", encodeListOptions(opts))
+	var out struct {
+		Installations []*installation `json:"installations"`
+	}
+	res, err := s.client.do(ctx, "GET", path, nil, &out)
+	return convertInstallationList(out.Installations), res, err
+}
+
+// helper function to convert from the Github repository list to
+// the common repository structure.
+func convertInstallationList(from []*installation) []*scm.Installation {
+	to := []*scm.Installation{}
+	for _, v := range from {
+		to = append(to, convertInstallation(v))
+	}
+	return to
+}
+
+// helper function to convert from the Github repository structure
+// to the common repository structure.
+func convertInstallation(from *installation) *scm.Installation {
+	return &scm.Installation{
+		ID:           strconv.Itoa(from.Id),
+		Organization: convertOrganization(from.Account),
+		AppName:      string(from.AppSlug),
+	}
 }
--- scm/installation.go
+++ scm/installation.go
@@ -4,8 +4,17 @@ import (
 	"context"
 )
 
+// Repository represents a git repository.
+
 type (
+	Installation struct {
+		ID           string
+		Organization *Organization
+		AppName      string
+	}
 	InstallationService interface {
 		ListRepositories(ctx context.Context, opts ListOptions) ([]*Repository, *Response, error)
+
+		ListInstallationsForAuthenticatedUser(ctx context.Context, opts ListOptions) ([]*Installation, *Response, error)
 	}
 )
