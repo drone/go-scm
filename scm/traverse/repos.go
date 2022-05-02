6d77e8582467a368223331c45c8a4f0d5776af96
Brad Rydzewski <bradley.rydzewski@harness.io>
2022-05-02 16:27:33 -0400

    add helper function fetch all repos

---
+++ scm/traverse/repos.go
@@ -0,0 +1,36 @@
+// Copyright 2022 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package traverse
+
+import (
+	"context"
+
+	"github.com/drone/go-scm/scm"
+)
+
+// Repos returns the full repository list, traversing and
+// combining paginated responses if necessary.
+func Repos(ctx context.Context, client *scm.Client, webhook *scm.Webhook) ([]*scm.Repository, error) {
+	list := []*scm.Repository{}
+	opts := scm.ListOptions{Size: 100}
+	for {
+		result, meta, err := client.Repositories.List(ctx, opts)
+		if err != nil {
+			return nil, err
+		}
+		for _, src := range result {
+			if src != nil {
+				list = append(list, result...)
+			}
+		}
+		opts.Page = meta.Page.Next
+		opts.URL = meta.Page.NextURL
+
+		if opts.Page == 0 && opts.URL == "" {
+			break
+		}
+	}
+	return list, nil
+}
