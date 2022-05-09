78b5e763f6cd368a16aecfca6c391c13e0c26017
Deepak Patankar <deepak.patankar@harness.io>
2022-05-09 16:37:57 +0530

    [feat]: [PL-24913]: Add the support for create and update file in bitbucket server (#177)
    * [PL-24913]: Add the support for create and update api in bitbucket server
    * [PL-24913]: Add the tests for create and update of file in bitbucket server

--- scm/driver/stash/integration/testSettings.go
+++ scm/driver/stash/integration/testSettings.go
@@ -1,16 +1,26 @@
 package integration
 
 import (
+	"context"
 	"os"
 
 	"github.com/drone/go-scm/scm"
 )
 
 var (
-	client   *scm.Client
-	token    = os.Getenv("BITBUCKET_SERVER_TOKEN")
+	client *scm.Client
+	token  = os.Getenv("BITBUCKET_SERVER_TOKEN")
+
 	endpoint = "https://bitbucket.dev.harness.io/"
 	repoID   = "har/scm-integration-test-repo"
 	username = "harnessadmin"
 	commitId = "f675c4b55841908d7c338c500c8f4cb844fd9be7"
 )
+
+func GetCurrentCommitOfBranch(client *scm.Client, branch string) (string, error) {
+	commits, _, err := client.Git.ListCommits(context.Background(), repoID, scm.CommitListOptions{Ref: branch})
+	if err != nil {
+		return "", err
+	}
+	return commits[0].Sha, nil
+}
