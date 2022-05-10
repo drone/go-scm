7a7b0ca149913c0bf901265c77761984a8198f2e
Mankrit Singh <mankrit.singh@harness.io>
2022-05-10 17:35:21 +0530

    Revert "[PL-25025]: Typo"
    This reverts commit 826531d392eb1c576cda2ec73b60a2f593958896.

--- scm/driver/azure/azure.go
+++ scm/driver/azure/azure.go
@@ -117,7 +117,7 @@ func (e *Error) Error() string {
 }
 
 func ProjectRequiredError() error {
-        return error.New("This API endpoint requires a project to be specified")
+        return errors.New("This API endpoint requires a project to be specified")
 }
 
 func SanitizeBranchName(name string) string {
