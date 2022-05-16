1ff5fea1a4598410eb408e598c95e15a21f163e2
DeepakPatankar <deepak.patankar@harness.io>
2022-05-16 15:45:22 +0530

    Add the support to fetch commit of a file

--- scm/driver/stash/integration/git_test.go
+++ scm/driver/stash/integration/git_test.go
@@ -34,5 +34,31 @@ func TestCreateBranch(t *testing.T) {
 	if response.Status != http.StatusOK {
 		t.Errorf("CreateBranch did not get a 200 back %v", response.Status)
 	}
+}
 
+func TestGetLatestCommitOfBranch(t *testing.T) {
+	if token == "" {
+		t.Skip("Skipping, Acceptance test")
+	}
+	client, _ = stash.New(endpoint)
+	client.Client = &http.Client{
+		Transport: &transport.BasicAuth{
+			Username: username,
+			Password: token,
+		},
+	}
+
+	commits, response, err := client.Git.ListCommits(context.Background(), repoID, scm.CommitListOptions{Ref: "master", Path: "README"})
+
+	if err != nil {
+		t.Errorf("GetLatestCommitOfFile got an error %v", err)
+	} else {
+		if response.Status != http.StatusOK {
+			t.Errorf("GetLatestCommitOfFile did not get a 200 back %v", response.Status)
+		}
+
+		if commits[0].Sha != "2cc4dbe084f0d66761318b305c408cb0ea300c9a" {
+			t.Errorf("Got the commitId %s instead of the top commit of the file", commits[0].Sha)
+		}
+	}
 }
