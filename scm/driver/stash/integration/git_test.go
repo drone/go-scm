8df3eed9cccaad82f72c686240178d9900117560
Mohit Garg <mohit.garg@harness.io>
2022-08-17 00:25:31 +0530

    added new integration test

--- scm/driver/stash/integration/git_test.go
+++ scm/driver/stash/integration/git_test.go
@@ -62,3 +62,30 @@ func TestGetLatestCommitOfBranch(t *testing.T) {
 		}
 	}
 }
+
+func TestGetLatestCommitOfNonDefaultBranch(t *testing.T) {
+	if token == "" {
+		t.Skip("Skipping, Acceptance test")
+	}
+	client, _ = stash.New(endpoint)
+	client.Client = &http.Client{
+		Transport: &transport.BasicAuth{
+			Username: username,
+			Password: token,
+		},
+	}
+
+	commits, response, err := client.Git.ListCommits(context.Background(), repoID, scm.CommitListOptions{Ref: "main", Path: "do-not-touch.txt"})
+
+	if err != nil {
+		t.Errorf("GetLatestCommitOfFile got an error %v", err)
+	} else {
+		if response.Status != http.StatusOK {
+			t.Errorf("GetLatestCommitOfFile did not get a 200 back %v", response.Status)
+		}
+
+		if commits[0].Sha != "76fb1762048a277596d3fa330b3da140cd12d361" {
+			t.Errorf("Got the commitId %s instead of the top commit of the file", commits[0].Sha)
+		}
+	}
+}
