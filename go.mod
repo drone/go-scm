0691038ca09101c4a03b3968a0cb97add5649832
Sabith KS <sabithksme@gmail.com>
2023-11-10 15:17:05 -0800

    Add option to get repos in parallel

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/drone/go-scm
 require (
 	github.com/google/go-cmp v0.2.0
 	github.com/h2non/gock v1.0.9
-	github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32 // indirect
+	golang.org/x/sync v0.5.0
 )
 
-go 1.13
+require github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32 // indirect
+
+go 1.21
--- scm/traverse/repos.go
+++ scm/traverse/repos.go
@@ -8,6 +8,7 @@ import (
 	"context"
 
 	"github.com/drone/go-scm/scm"
+	"golang.org/x/sync/errgroup"
 )
 
 // Repos returns the full repository list, traversing and
@@ -20,11 +21,7 @@ func Repos(ctx context.Context, client *scm.Client) ([]*scm.Repository, error) {
 		if err != nil {
 			return nil, err
 		}
-		for _, src := range result {
-			if src != nil {
-				list = append(list, src)
-			}
-		}
+		list = addNonNil(list, result)
 		opts.Page = meta.Page.Next
 		opts.URL = meta.Page.NextURL
 
@@ -33,4 +30,41 @@ func Repos(ctx context.Context, client *scm.Client) ([]*scm.Repository, error) {
 		}
 	}
 	return list, nil
+}
+
+// ReposV2 same as Repos but uses errgroup to fetch repos in parallel
+func ReposV2(ctx context.Context, client *scm.Client) ([]*scm.Repository, error) {
+	list := []*scm.Repository{}
+	opts := scm.ListOptions{Size: 100}
+
+	result, meta, err := client.Repositories.List(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+	list = addNonNil(list, result)
+	if meta.Page.Next == 0 && meta.Page.NextURL == "" {
+		return list, nil
+	}
+	errGroup, ectx := errgroup.WithContext(ctx)
+	for i := meta.Page.Next; i <= meta.Page.Last; i++ {
+		opts := scm.ListOptions{Size: 100, Page: i}
+		errGroup.Go(func() error {
+			result, _, err := client.Repositories.List(ectx, opts)
+			if err != nil {
+				return err
+			}
+			list = addNonNil(list, result)
+			return nil
+		})
+	}
+	return list, errGroup.Wait()
+}
+
+func addNonNil(list []*scm.Repository, result []*scm.Repository) []*scm.Repository {
+	for _, src := range result {
+		if src != nil {
+			list = append(list, src)
+		}
+	}
+	return list
 }
