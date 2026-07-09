ec480fc7ea6148cee6cf66f59e9c89173545d6aa
Yash Verma <yash.verma@harness.io>
2026-07-09 18:59:24 +0000

    feat: [STO-11975]: Implemented review comment (#100011)
    * f9d7d0 feat:[STO-11846]:Removed Patch
    
    * f34cd3 feat:[STO-11846]:Reverted complex patches
    
    * ac053a feat:[STO-11864]: add diff parsing/applying unit tests
    
    * fc8bd9 feat:[STO-11864]: revered non-required changes
    
    * 6e2540 feat:[STO-11864]: Added review link
    
    * b7a40d added bug fix
    
    * 6085ab added bug fix
    
    * 6d7427 new commit
    
    * 1ed16e new commit
    
    * e85589 added review

--- scm/driver/harness/review.go
+++ scm/driver/harness/review.go
@@ -6,6 +6,9 @@ package harness
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/drone/go-scm/scm"
 )
@@ -23,9 +26,58 @@ func (s *reviewService) List(ctx context.Context, repo string, number int, opts 
 }
 
 func (s *reviewService) Create(ctx context.Context, repo string, number int, input *scm.ReviewInput) (*scm.Review, *scm.Response, error) {
-	return nil, nil, scm.ErrNotSupported
+	harnessURI := buildHarnessURI(s.client.account, s.client.organization, s.client.project, repo)
+	repoId, queryParams, err := getRepoAndQueryParams(harnessURI)
+	if err != nil {
+		return nil, nil, err
+	}
+	path := fmt.Sprintf("api/v1/repos/%s/pullreq/%d/comments?%s", repoId, number, queryParams)
+
+	in := &prComment{
+		Text:            input.Body,
+		Path:            input.Path,
+		SourceCommitSha: input.Sha,
+		ParentID:        input.InReplyTo,
+		LineEnd:         input.Line,
+		LineEndNew:      input.Side != scm.SideLeft,
+	}
+	if input.StartLine > 0 {
+		startSide := input.StartSide
+		if startSide == scm.SideUnspecified {
+			startSide = input.Side
+		}
+		in.LineStart = input.StartLine
+		in.LineStartNew = startSide != scm.SideLeft
+	} else {
+		in.LineStart = input.Line
+		in.LineStartNew = in.LineEndNew
+	}
+
+	out := new(prCommentResponse)
+	res, err := s.client.do(ctx, "POST", path, in, out)
+	return convertReviewComment(out, input), res, err
 }
 
 func (s *reviewService) Delete(ctx context.Context, repo string, number, id int) (*scm.Response, error) {
 	return nil, scm.ErrNotSupported
+}
+
+func convertReviewComment(from *prCommentResponse, input *scm.ReviewInput) *scm.Review {
+	return &scm.Review{
+		ID:   from.Id,
+		Body: from.Text,
+		Path: input.Path,
+		Sha:  input.Sha,
+		Line: input.Line,
+		Author: scm.User{
+			ID:      strconv.Itoa(from.Author.Id),
+			Login:   from.Author.Uid,
+			Name:    from.Author.DisplayName,
+			Email:   from.Author.Email,
+			Created: time.UnixMilli(from.Author.Created),
+			Updated: time.UnixMilli(from.Author.Updated),
+		},
+		Created: time.UnixMilli(from.Created),
+		Updated: time.UnixMilli(from.Updated),
+	}
 }
